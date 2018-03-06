519734008065d443d89e529a991ba14e3146abd7
HCMY <yuanchenzhouhcmy@gmail.com>
2018-03-06 17:35:16 +0800

    replace test_value

---
+++ ycimpute/utils/shower/__init__.py
@@ -0,0 +1,11 @@
+"""
+from ...imputer.mice import MICE
+from ...imputer.knnimput import KNN
+from ...imputer.iterforest import IterImput
+from ...imputer.simple import SimpleFill
+
+__all__=["MICE",
+         "KNN",
+         "IterImput",
+         "SimpleFill"]
+"""
